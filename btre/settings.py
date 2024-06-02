8e54d130df97a93c20bac266cebe9aa144f402d9
Waliullah007 <123813287+Waliullah007@users.noreply.github.com>
2024-06-02 23:33:36 +0000

    Update settings.py

--- btre/settings.py
+++ btre/settings.py
@@ -188,9 +188,3 @@ MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.example.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'waliullahtaier007@gmail.com'
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
